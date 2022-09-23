81bebe8a181cd5916f50a28f33d31444f9b796e4
capogreco <thomas.capogreco@gmail.com>
2022-09-23 17:26:10 +1000

    added return in servePublic function definition

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ import { WebSocketServer } from "https://deno.land/x/websocket@v0.1.4/mod.ts"
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts"
 
 // serve public
-const servePublic = req => staticFiles ('public') ({ 
+const servePublic = req => {
+    return staticFiles ('public')
+} ({ 
     request: req, 
     respondWith: r => r 
 })
