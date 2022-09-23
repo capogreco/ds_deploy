5f500f8f237bd6e55b297f1945e7273098905140
capogreco <thomas.capogreco@gmail.com>
2022-09-24 07:14:44 +1000

    simplified serveDir

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import { serve } from "https://deno.land/std@$STD_VERSION/http/server.ts";
 import { serveDir } from "https://deno.land/std@$STD_VERSION/http/file_server.ts";
 
-serve((req) => {
-    const pathname = new URL(req.url).pathname;
-    if (pathname.startsWith (`/`)) {
-        return serveDir (req, {
-            fsRoot: `public`,
-        })
-    }
-    // Do dynamic responses
-    return new Response();
-});
+// serve((req) => {
+//     const pathname = new URL(req.url).pathname;
+//     if (pathname.startsWith (`/`)) {
+//         return serveDir (req, {
+//             fsRoot: `public`,
+//         })
+//     }
+//     // Do dynamic responses
+//     return new Response();
+// });
 
 
+serve (req => serveDir (req, { fsRoot: `public`}))
