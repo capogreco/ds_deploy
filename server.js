316932b4ff8dcca6aead393284442fcd012a79c1
capogreco <thomas.capogreco@gmail.com>
2022-09-23 23:17:38 +1000

    shortened url for serveFile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 
 serve(async (req) => {
-  return await serveFile (req, `${Deno.cwd()}/public/index.html`);
+  return await serveFile (req, `${Deno.cwd()}/public`);
 })
 
 
