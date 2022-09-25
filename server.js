bb9d4934a56b6b91d86c9fcdf40649837436e54a
capogreco <thomas.capogreco@gmail.com>
2022-09-25 19:43:52 +1000

    if statement in serve function def

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts";
 
-serve (req => serveDir (req, { fsRoot: `public`}))
+// serve (req => serveDir (req, { fsRoot: "public" }))
 
 
 
-// serve((req) => {
-//     const pathname = new URL(req.url).pathname;
-//     if (pathname.startsWith (`/`)) {
-//         return serveDir (req, {
-//             fsRoot: `public`,
-//         })
-//     }
-//     // Do dynamic responses
-//     return new Response();
-// });
+serve((req) => {
+    const pathname = new URL(req.url).pathname;
+    if (pathname.startsWith (`/`)) {
+        return serveDir (req, {
+            fsRoot: `public`,
+        })
+    }
+    // Do dynamic responses
+    return new Response();
+});
 
 
