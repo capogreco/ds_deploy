fe34ac50a31dab306a36b3717358bae2b7cd63f4
capogreco <thomas.capogreco@gmail.com>
2022-09-27 20:10:57 +1000

    control and public no directories

--- server.js
+++ server.js
@@ -1,19 +1,34 @@
 import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts";
 
+const req_handler = async (req, con) => {
+    // const { pathname: path } = new URL(req.url)
+    const path = new URL (req.url).pathname
+
+    switch (path) {
+        case "/control":
+            return new Response ("this is control", { status: 404 });
+        case "/":
+            return new Response ("this is public", { status: 404 });
+        }
+
+    return new Response ("hello hello")
+}
+
+serve (req_handler, { port: 80 })
+
+
 // serve (req => serveDir (req, { fsRoot: "public" }))
 
-
-
-serve((req) => {
-    const pathname = new URL(req.url).pathname;
-    if (pathname.startsWith (`/`)) {
-        return serveDir (req, {
-            fsRoot: `public`,
-        })
-    }
-    // Do dynamic responses
-    return new Response();
-});
+// serve((req) => {
+//     const pathname = new URL(req.url).pathname;
+//     if (pathname.startsWith (`/`)) {
+//         return serveDir (req, {
+//             fsRoot: `public`,
+//         })
+//     }
+//     // Do dynamic responses
+//     return new Response();
+// });
 
 
