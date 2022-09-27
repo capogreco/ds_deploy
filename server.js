aeef339506314b38feee0bee8b41e4c9d5e40f17
capogreco <thomas.capogreco@gmail.com>
2022-09-27 20:31:37 +1000

    using serveFile instead of serveDir

--- server.js
+++ server.js
@@ -1,34 +1,17 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.157.0/http/file_server.ts";
+import { serve } from "https://deno.land/std@0.157.0/http/server.ts"
+import { serveFile } from "https://deno.land/std@0.157.0/http/file_server.ts?s=serveFile"
 
 const req_handler = async (req, con) => {
-    // const { pathname: path } = new URL(req.url)
     const path = new URL (req.url).pathname
 
     switch (path) {
         case "/control":
-            return new Response ("this is control", { status: 404 });
+            return serveFile (req, `control/index.html`)
         case "/":
-            return new Response ("this is public", { status: 404 });
+            return serveFile (req, `public/index.html`)
         }
 
     return new Response ("hello hello")
 }
 
 serve (req_handler, { port: 80 })
-
-
-// serve (req => serveDir (req, { fsRoot: "public" }))
-
-// serve((req) => {
-//     const pathname = new URL(req.url).pathname;
-//     if (pathname.startsWith (`/`)) {
-//         return serveDir (req, {
-//             fsRoot: `public`,
-//         })
-//     }
-//     // Do dynamic responses
-//     return new Response();
-// });
-
-
